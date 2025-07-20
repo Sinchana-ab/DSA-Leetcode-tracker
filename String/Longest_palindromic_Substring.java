1881f477ae06277da1bc022b0bd0ea87c2dfc0fe
sinchana-ab <sinchanaab.parvam@gmail.com>
2025-07-20 09:00:10 +0530

    palindrome

---
+++ String/Longest_palindromic_Substring.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPalindrome(String s) {
+         int n = s.length();
+        if(s == null || n<1){
+            return "";
+        }
+       
+        String longest = "";
+        for(int i = 0;i<n;i++){
+            for(int j = i ;j< n;j++){
+                if(ispalindrome(s,i,j) && (j-i+1)>longest.length()){
+                    longest = s.substring(i,j+1);
+                }
+            }
+        }
+        return longest;
+    }
+    boolean ispalindrome(String s,int start,int end){
+        while(start<end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
